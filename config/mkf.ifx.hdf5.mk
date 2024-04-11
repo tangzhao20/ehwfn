b58b8e49e94e95c5646097e600d7b8836f10d461
Zhao Tang <tangzhao20@gmail.com>
2024-04-11 11:10:48 -0500

    Added the config file for stampede3

---
+++ config/mkf.ifx.hdf5.mk
@@ -0,0 +1,18 @@
+F90     = ifx
+
+FPP     = ifx -EP
+
+DEFS  = -DHDF5 
+
+FOPTS    = -O3
+
+LIBS    = 
+
+HDF5_LDIR = -L$(HDF5_DIR)/lib
+HDF5LIB = -lhdf5hl_fortran -lhdf5_hl -lhdf5_fortran -lhdf5 -lz -ldl 
+HDF5INCLUDE = -I$(HDF5_DIR)/include
+
+LIBS += $(HDF5_LDIR) $(HDF5LIB)
+F90 += $(HDF5INCLUDE)
+
+LINK = $(F90)
